3851935105846d5f2eff9f644ce6bb8b3fa9425f
Choi Chang Hwan <cghy4862@gmail.com>
2022-03-15 15:09:40 +0900

    ADD: Owner/Dog Get기능 추가

--- owners/views.py
+++ owners/views.py
@@ -18,10 +18,25 @@ class OwnerView(View):
 
         return JsonResponse({'message':'created'}, status=201)
 
+    
+    def get(self, request):
+        owners = Owner.objects.all()
+        results = []
+
+        for owner in owners:
+            results.append(
+                {
+                    "name" : owner.name,
+                    "email" : owner.email,
+                    "age" : owner.age
+                }
+            )
+
+        return JsonResponse({'reults':results}, status=200)
+
 class DogView(View):
     def post(self, request):
         data = json.loads(request.body)
-
         owner = Owner.objects.get(name=data['owner'])
         dog = Dog.objects.create(
             name = data['name'],
@@ -31,3 +46,18 @@ class DogView(View):
 
         return JsonResponse({'message':'created'}, status=201)
 
+
+    def get(self, request):
+        dogs = Dog.objects.all()
+        results = []
+
+        for dog in dogs:
+            results.append(
+                {
+                    "name" : dog.name,
+                    "age" : dog.age,
+                    "owner" : dog.owner.name
+                }
+            )
+
+        return JsonResponse({'reults':results}, status=200)
